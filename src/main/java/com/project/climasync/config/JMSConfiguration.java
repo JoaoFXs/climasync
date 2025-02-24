e31a3f4eb4ce88e4a3f0814d2ba4746e0659ceae
jovibfel@gmail.com <jovibfel@gmail.com>
2025-02-24 20:34:25 -0300

    Adjust in file JMSConfiguration

--- src/main/java/com/project/climasync/config/JMSConfiguration.java
+++ src/main/java/com/project/climasync/config/JMSConfiguration.java
@@ -1,6 +1,7 @@
 package com.project.climasync.config;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
@@ -12,17 +13,23 @@ import jakarta.jms.ConnectionFactory;
 @Configuration
 @EnableJms
 public class JMSConfiguration {
+	
+	@Value("${activemq.user}")
+	private String activemqUser;
+	
+	@Value("${activemq.user}")
+	private String activemqPwd;
+	
+	@Value("${activemq.url}")
+	private String activemqUrl;
     
-    private static final String BROKER_URL = "tcp://localhost:61616";
-    private static final String BROKER_USERNAME = "admin";
-    private static final String BROKER_PASSWORD = "admin";
-    
+
     @Bean
     public ConnectionFactory connectionFactory() {
         ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
-        factory.setBrokerURL(BROKER_URL);
-        factory.setUserName(BROKER_USERNAME);
-        factory.setPassword(BROKER_PASSWORD);
+        factory.setBrokerURL(activemqUrl);
+        factory.setUserName(activemqUser);
+        factory.setPassword(activemqPwd);
         return factory;
     }
     
